b7818e52db921b3fc38fb6ca93ea11ee22816ab7
Martin Delius <martin.delius@gmail.com>
2011-02-07 18:47:19 +0100

    fixed an remade layout and text for 404-error-template

--- 404.php
+++ 404.php
@@ -1,10 +1,16 @@
 <?php get_header(); ?>
 
-	<div id="content" class="narrowcolumn">
+	<div class="blog module">
 
-		<h2 class="center"><?php _e('Error 404 - Not Found', 'kubrick'); ?></h2>
-
-	</div>
+		<div id="error">
+			<h2><?php _e('Error 404 - Die Seite existiert leider nicht.', 'kubrick'); ?></h2>
+			<p> 
+				Die abgefragte Seite ist leider nicht mehr oder noch nicht online. Das ist kein Problem und sicher unser Fehler. Wenn du unsere Seiten weiter durchstöbern möchest, kannst du einfach unter <a href="<?php echo get_option('home'); ?>/">unserer Startseite</a> weitermachen.
+			</p>
+			<p> Viel Spass!</a>
+		</div> <!--404-->
+	</div> <!-- blog module -->
+	</div> <!-- col1 -->
 
 <?php get_sidebar(); ?>
 
