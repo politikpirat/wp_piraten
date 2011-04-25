302cedb1e86ba9c4b924e9841a70be6da1db7f0d
Martin Delius <martin.delius@gmail.com>
2011-04-25 18:59:47 +0200

    restoring piratewidgets.php

--- pirateWidgets.php
+++ pirateWidgets.php
@@ -1,5 +1,12 @@
-<?php
+rateWidgets
+ * @author Florian Bischof
+ */
 
+/**
+ * Pirates Everywhere
+ * A link list with small images
+ * @param mixed $args
+ */
 function piratesEverywhere($args) {
 	print <<<END
 				<div class="box boxPirateCommunities" id="">
@@ -7,18 +14,20 @@ function piratesEverywhere($args) {
 					<div class="content linklist">
 						<table id="linkTable"><tr><td>
 							<div id="twitterLink"><a href="http://twitter.com/piratenberlin">Twitter</a></div>
-							<div id="thepiratebayLink"><a href="http://thepiratebay.org/user/Hoshpak/">The Pirate Bay</a></div>
+                                                        <div id="identicaLink"><a href="http://identi.ca/piratenberlin">Identi.ca</a></div>
 							<div id="myspaceLink"><a href="http://www.myspace.com/piratenparteiberlin">MySpace</a></div>
 							<div id="youtubeLink"><a href="http://www.youtube.com/user/Piratenpartei">YouTube</a></div>
+                                                </td><td>
 							<div id="flickrLink"><a href="http://www.flickr.com/search/?q=piratenpartei&w=all">Flickr</a></div>
 							<div id="diggLink"><a href="http://digg.com/search?s=piratenpartei">Digg</a></div>
-						</td><td>
-							<div id="facebookLink"><a href="http://www.facebook.com/group.php?gid=19095902528">Facebook</a></div>
-							<div id="meinvzLink">MeinVZ</div>
+                                                        <div id="facebookLink"><a href="http://www.facebook.com/pages/Piratenpartei-Berlin/111078392276328">Facebook</a></div>
+						</td>
+<!--                                            <td>						        
+                                                        <div id="meinvzLink">MeinVZ</div>
 							<div id="schuelervzLink">SchülerVZ</div>
 							<div id="studivzLink">StudiVZ</div>
 							<div id="werkenntwenLink">Wer kennt wen</div>
-<!--							<div id="Link"><a href="#"></a></div>
+							<div id="Link"><a href="#"></a></div>
 							<div id="eventfulLink">Eventful</div>
 							<div id="linkedInLink">LinkedIn</div>
 							<div id="Link"><a href="#">BlackPlanet</a></div>
@@ -27,9 +36,10 @@ function piratesEverywhere($args) {
 							<div id="Link"><a href="#">Glee</a></div>
 							<div id="Link"><a href="#">MiGente</a></div>
 							<div id="Link"><a href="#">MyBatanga</a></div>
-							<div id="Link"><a href="#">DNC Partybuilder</a></div>-->
-							<div id="gayromeoLink"><a href="http://www.gayromeo.com/PIRATEN">GayRomeo</a></div>
-						</td></tr></table>
+							<div id="Link"><a href="#">DNC Partybuilder</a></div>
+							<div id="gayromeoLink"><a href="#">GayRomeo</a></div>
+							<div id="thepiratebayLink"><a href="http://thepiratebay.org/user/Hoshpak/">The Pirate Bay</a></div>
+						</td>--></tr></table>
 					</div>
 				</div>
 END;
@@ -47,27 +57,43 @@ wp_register_widget_control('piratesEverywhere', 'Piraten Überall', 'piratesEver
 
 
 
-//// SPENDEN ZEUGS
-
+/**
+ * Pirate Donation Progress
+ * Displays a donation progress bar
+ * widget_pirate_donation_progress options:
+ * - headline (default "Donation")
+ * - description (default "Please donate")
+ * - link (default #)
+ * - currentValue (default 23)
+ * - targetValue (default 42)
+ * - endDate
+ * @param mixed $args
+ */
 function pirateDonationProgress($args) {
 	extract($args);
 	$options = get_option('widget_pirate_donation_progress');
+	$headline = ($options['headline']) ? $options['headline'] : 'Donation';
+	$description = ($options['description']) ? $options['description'] : 'Please donate';
+	$link = ($options['link']) ? $options['link'] : '#';
 	$currentValue = ($options['currentValue']) ? $options['currentValue'] : 23;
 	$targetValue = ($options['targetValue']) ? $options['targetValue'] : 42;
 	$daysLeft = ($options['endDate']) ? floor((strtotime($options['endDate']) - time()) / 86400) : 42;
 	$percent = floor(($currentValue / $targetValue) * 100);
 	
 	print <<<END
-		<div id="boxVote" class="box">
-			<h2>Spenden</h2>
+		<div id="boxDonation" class="box">
+			<h2>{$headline}</h2>
 			<div class="content">
-				<a href="http://berlin.piratenpartei.de/?page_id=1013">Aktuell benötigen wir noch Geld für den Außenboarders unserer Wahlkampffloßes!</a>
+				<a href="{$link}">{$description}</a>
 				<div class="box boxBarometer">
 					<div class="progressbar"><div class="progress" style="width:{$percent}%"></div></div>
 					Stand: <span class="boxBarometerCurrent">{$currentValue} €</span> von
 					<span class="boxBarometerTarget">{$targetValue} €</span>. Noch <strong>{$daysLeft}</strong> Tage!
 				</div>
 			</div>
+                        <div id=sidebtn>
+                                <center><a href="http://berlin.piratenpartei.de/partei/impressum/spenden">jetzt spenden...</a></center>
+                        </div>
 		</div>	
 END;
 
@@ -77,6 +103,9 @@ function pirateDonationProgressControl() {
 	$options = $newoptions = get_option('widget_pirate_donation_progress');
 
 	if ( isset($_POST['pirateDonationProgress-submit']) ) {
+		$newoptions['headline'] = strip_tags(stripslashes($_POST['pirateDonationProgressHeadline']));
+		$newoptions['description'] = strip_tags(stripslashes($_POST['pirateDonationProgressDescription']));
+		$newoptions['link'] = strip_tags(stripslashes($_POST['pirateDonationProgressLink']));
 		$newoptions['currentValue'] = strip_tags(stripslashes($_POST['pirateDonationProgressCurrentValue']));
 		$newoptions['targetValue'] = strip_tags(stripslashes($_POST['pirateDonationProgressTargetValue']));
 		$newoptions['endDate'] = strip_tags(stripslashes($_POST['pirateDonationProgressEndDate']));
@@ -87,11 +116,23 @@ function pirateDonationProgressControl() {
 		update_option('widget_pirate_donation_progress', $options);
 	}
 
+	$headline =  attribute_escape( $options['headline'] );
+	$description =  attribute_escape( $options['description'] );
+	$link =  attribute_escape( $options['link'] );	
 	$currentValue = attribute_escape( $options['currentValue'] );
 	$targetValue = attribute_escape( $options['targetValue'] );
 	$endDate = attribute_escape( $options['endDate'] );
 
 ?>
+	<p><label for="pirateDonationProgressHeadline">
+	Headline: <input type="text" class="widefat" id="pirateDonationProgressHeadline" name="pirateDonationProgressHeadline" value="<?php echo $headline ?>" /></label>
+	</p>
+	<p><label for="pirateDonationProgressDescription">
+	Description: <input type="text" class="widefat" id="pirateDonationProgressDescription" name="pirateDonationProgressDescription" value="<?php echo $description ?>" /></label>
+	</p>
+	<p><label for="pirateDonationProgressLink">
+	Link: <input type="text" class="widefat" id="pirateDonationProgressLink" name="pirateDonationProgressLink" value="<?php echo $link ?>" /></label>
+	</p>
 	<p><label for="pirateDonationProgressCurrentValue">
 	Aktueller Stand: <input type="text" class="widefat" id="pirateDonationProgressCurrentValue" name="pirateDonationProgressCurrentValue" value="<?php echo $currentValue ?>" /></label>
 	</p>
@@ -99,7 +140,7 @@ function pirateDonationProgressControl() {
 	Ziel: <input type="text" class="widefat" id="pirateDonationProgressTargetValue" name="pirateDonationProgressTargetValue" value="<?php echo $targetValue ?>" /></label>
 	</p>
 	<p><label for="pirateDonationProgressEndDate">
-	Aktueller Stand: <input type="text" class="widefat" id="pirateDonationProgressEndDate" name="pirateDonationProgressEndDate" value="<?php echo $endDate ?>" /></label>
+	Deadline: <input type="text" class="widefat" id="pirateDonationProgressEndDate" name="pirateDonationProgressEndDate" value="<?php echo $endDate ?>" /></label>
 	</p>
 	<input type="hidden" name="pirateDonationProgress-submit" id="pirateDonationProgress-submit" value="1" />
 <?php
@@ -107,10 +148,18 @@ function pirateDonationProgressControl() {
 wp_register_sidebar_widget('pirateDonationProgress', 'Piraten Spendensammlungs Fortschritt', 'pirateDonationProgress');
 wp_register_widget_control('pirateDonationProgress', 'Piraten Spendensammlungs Fortschritt', 'pirateDonationProgressControl' );
 
-//// /SPENDEN ZEUGS
 
 
 
+/**
+ * Pirate Collection Progress
+ * Displays a collection progress bar
+ * widget_pirate_collection_progress options:
+ * - currentValue (default 23)
+ * - targetValue (default 42)
+ * - endDate
+ * @param mixed $args
+ */
 function pirateCollectionProgress($args) {
 	extract($args);
 	$options = get_option('widget_pirate_collection_progress');
@@ -132,20 +181,6 @@ function pirateCollectionProgress($args) {
 			</div>
 		</div>	
 END;
-
-/*
-	<script type="text/javascript" charset="utf-8">
-		var progressEl = document.getElementsByClassName('progress')[0];
-		var currentWidth = progressEl.style.width;
-		progressEl.style.width = 0;
-		console.log('currentWidth', currentWidth);
-		 window.setTimeout(function() {
-				progressEl.style.width = currentWidth;
-		}, 600);
-
-	</script>
-*/
-
 }
 
 function pirateCollectionProgressControl() {
@@ -174,7 +209,7 @@ function pirateCollectionProgressControl() {
 	Ziel: <input type="text" class="widefat" id="pirateCollectionProgressTargetValue" name="pirateCollectionProgressTargetValue" value="<?php echo $targetValue ?>" /></label>
 	</p>
 	<p><label for="pirateCollectionProgressEndDate">
-	Aktueller Stand: <input type="text" class="widefat" id="pirateCollectionProgressEndDate" name="pirateCollectionProgressEndDate" value="<?php echo $endDate ?>" /></label>
+	Deadline: <input type="text" class="widefat" id="pirateCollectionProgressEndDate" name="pirateCollectionProgressEndDate" value="<?php echo $endDate ?>" /></label>
 	</p>
 	<input type="hidden" name="pirateCollectionProgress-submit" id="pirateCollectionProgress-submit" value="1" />
 <?php
@@ -185,12 +220,16 @@ wp_register_widget_control('pirateCollectionProgress', 'Piraten Sammlungs Fortsc
 
 
 
+/**
+ * Pirate SMS
+ * @param mixed $args
+ */
 function pirateWidgetSms($args) {
 	extract($args);
 	$options = get_option('widget_pirate_sms');
 
 	echo '<div class="box info" id="boxBecomePirateSms">';
-	echo '<a href="#"><strong>Werde SMS Pirat!</strong><span>Sende <q>PIRAT</q> an 11825</span></a>';
+	echo '<a href="#"><strong>Werde SMS Pirat!</strong><span>Sende <q>PIRAT</q> an 0163 7774728</span></a>';
 	echo '</div>';
 
 }
@@ -222,3 +261,4 @@ wp_register_widget_control('pirateSms', 'SMS Pirat', 'pirateWidgetSmsControl' );
 
 
 ?>
+
